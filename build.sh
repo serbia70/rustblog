87ecd9ec4299f5bbf657351968a348521eaf4117
serbia70 <serbia1688@gmail.com>
2026-05-31 10:07:59 +0200

    Fix build.sh AVIF patches for Vercel deployment
    Add missing patches for _macros.html and loading-box.html
    so Zola build succeeds on Vercel.

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ git submodule update --init --recursive
 
 # patch: theme uses avif but zola binary doesn't support it
 sed -i 's/format="avif"/format="webp"/g' themes/jiaxiang-wang/templates/shortcodes/image.html
-sed -i 's/format="avif"/format="webp"/g' themes/jiaxiang-wang/templates/macros/macros.html
+sed -i 's/format="avif"/format="webp"/g' themes/jiaxiang-wang/templates/_macros.html
+sed -i 's/format="avif"/format="webp"/g' themes/jiaxiang-wang/templates/partial/nav/loading-box.html
 
 curl -sL https://github.com/getzola/zola/releases/download/v0.19.2/zola-v0.19.2-x86_64-unknown-linux-gnu.tar.gz | tar xz
 ./zola build
